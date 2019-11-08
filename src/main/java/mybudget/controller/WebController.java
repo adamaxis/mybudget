379be586af6c02656c77c252d4d496f7e3a2b917
LeidyRussell <leidysemail@gmail.com>
2019-11-09 00:15:44 +0530

    add data  to database

--- src/main/java/mybudget/controller/WebController.java
+++ src/main/java/mybudget/controller/WebController.java
@@ -1,8 +1,15 @@
 package mybudget.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import mybudget.beans.Expense;
 import mybudget.repository.ExpensesRepository;
 
 @Controller
@@ -10,5 +17,18 @@ public class WebController {
 
 	@Autowired
 	ExpensesRepository repo;
+	
+	@GetMapping("/addBudget")
+	public String addBudgetForm(@ModelAttribute("expenses") ArrayList<Expense> expenses) 
+	{
+		return "create-budget"; 
+	}
+	
+	@PostMapping("/addExpenses")
+	public String addExpensesToDb(@ModelAttribute("expenses") ArrayList<Expense> expenses, Model model) { 
+		repo.saveAll(expenses);
+		model.addAttribute("expenses", repo.findAll());  
+		return "results";
+	}
 
 }
