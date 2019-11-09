3f6d4728511fc3a4531a09ca0c86dbc9b901825b
zherting <zherting@gmail.com>
2019-11-09 16:24:35 -0600

    Add User input and result section

--- src/main/java/mybudget/beans/Expense.java
+++ src/main/java/mybudget/beans/Expense.java
@@ -10,16 +10,20 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="expense")
+@Table(name = "expense")
 public class Expense {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long expense_id;
 	private LocalDate date_time;
 	private String name;
 	private double amount;
-	
+	@ManyToOne
+	private User user;
+	@ManyToOne
+	private Category category;
+
 	public Expense(LocalDate date_time, String name, double amount, User user, Category category) {
 		super();
 		this.date_time = date_time;
@@ -29,12 +33,6 @@ public class Expense {
 		this.category = category;
 	}
 
-	@ManyToOne
-	private User user;
-	
-	@ManyToOne
-	private Category category;
-	
 	public Expense() {
 		super();
 	}
@@ -92,5 +90,5 @@ public class Expense {
 		return "Expense [expense_id=" + expense_id + ", date_time=" + date_time + ", name=" + name + ", amount="
 				+ amount + ", user=" + user + ", category=" + category + "]";
 	}
-	
+
 }
--- src/main/java/mybudget/controller/WebController.java
+++ src/main/java/mybudget/controller/WebController.java
@@ -10,13 +10,39 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import mybudget.beans.Expense;
+import mybudget.beans.User;
 import mybudget.repository.ExpensesRepository;
+import mybudget.repository.UserRepository;
 
 @Controller
 public class WebController {
 
 	@Autowired
 	ExpensesRepository repo;
+	@Autowired
+	UserRepository repo2;
+	
+	@GetMapping("/viewAll")
+	public String viewAllUsers(Model model) {
+		model.addAttribute("users", repo2.findAll());
+		return "results";
+	}
+	
+	@GetMapping("/addUser")
+	public String addNewUser(Model model) {
+		User u = new User();
+		model.addAttribute("newUser", u);
+		return "addUser";
+	}
+	
+	@PostMapping("/addUser")
+	public String addNewUser(@ModelAttribute User u, Model model) {
+		repo2.save(u);
+		model.addAttribute("users", repo2.findAll());
+		return "results";
+	}
+	
+	
 	
 	@GetMapping("/addBudget")
 	public String addBudgetForm(@ModelAttribute("expenses") ArrayList<Expense> expenses) 
