6158420e1089e06fb9e375e856016309b712a51f
Draper, Daniel <ddraper@dmacc.edu>
2019-11-14 08:20:42 -0600

    Added search controller actions + page + repository queries

--- src/main/java/mybudget/repository/UserRepository.java
+++ src/main/java/mybudget/repository/UserRepository.java
@@ -1,9 +1,21 @@
 package mybudget.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import mybudget.beans.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long>{ }
+public interface UserRepository extends JpaRepository<User,Long>{
+	
+	  @Query("select u from User u where u.first_name = :first_name")
+	  List<User> findByFirstName(@Param("first_name") String first_name);
+	  @Query("select u from User u where u.last_name = :last_name")
+	  List<User> findByLastName(@Param("last_name") String last_name);
+	  @Query("select u from User u where u.email = :email")
+	  List<User> findByEmail(@Param("email") String email);
+}
