cb7e4632481f07cbbb5d33ed22a2dcde8124f25c
zherting <zherting@gmail.com>
2019-11-21 02:04:20 -0600

    Update "edit-expense" section

--- src/main/java/mybudget/controller/WebController.java
+++ src/main/java/mybudget/controller/WebController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.aspectj.bridge.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import mybudget.beans.Expense;
 import mybudget.beans.User;
 import mybudget.repository.UserRepository;
@@ -31,119 +33,153 @@ public class WebController {
 	@Autowired
 	UserRepository repo;
 
-	//launch results page showing all data
+	// launch results page showing all data
 	@GetMapping("/viewAll")
 	public String viewAllBudget(@ModelAttribute("user") User user, Model model) {
 		model.addAttribute("users", repo.findAll());
 		return "results";
 	}
-	
-	
+
 	@GetMapping("/addbudget")
-	public String addBudgetForm(@ModelAttribute("user") User user)
-	{
-		
-		return "create-budget"; 
+	public String addBudgetForm(@ModelAttribute("user") User user) {
+
+		return "create-budget";
 	}
-	
+
 	@PostMapping("/savebudget")
-	public String saveUserBudget(@ModelAttribute("user") User user, Model model) { 
+	public String saveUserBudget(@ModelAttribute("user") User user, Model model) {
 		repo.save(user);
-		model.addAttribute("user", repo.findAll());  
+		model.addAttribute("user", repo.findAll());
 		return "results";
 	}
-	
+
 	@GetMapping("/edituser/{id}")
-	public String editUserForm(@PathVariable("id") long id, Model model) 
-	{
+	public String editUserForm(@PathVariable("id") long id, Model model) {
 		User usr = repo.findById(id).orElse(null);
-		if(usr == null) return "error"; // error page goes here
+		if (usr == null)
+			return "error"; // error page goes here
 		model.addAttribute("user", usr);
-		return "edit-user"; 
+		return "edit-user";
 	}
-	
+
 	@GetMapping("/searchForUser")
-	public String searchForUser(Model model) 
-	{
-		return "search-for-user"; 
+	public String searchForUser(Model model) {
+		return "search-for-user";
 	}
-	
+
 	@RequestMapping(value = "/searchForUser", method = RequestMethod.POST)
-	public String saveUserForm(Model model, @RequestParam(value="searchText") String searchText, @RequestParam(value="searchType") String searchType)
-	{
+	public String saveUserForm(Model model, @RequestParam(value = "searchText") String searchText,
+			@RequestParam(value = "searchType") String searchType) {
 		List<User> users = null;
-		if(searchType.equals("first_name")) {
+		if (searchType.equals("first_name")) {
 			users = repo.findByFirstName(searchText);
-		} else if(searchType.equals("last_name")) {
+		} else if (searchType.equals("last_name")) {
 			users = repo.findByLastName(searchText);
-		} else if(searchType.equals("email")) {
+		} else if (searchType.equals("email")) {
 			users = repo.findByEmail(searchText);
 		}
-		
-		if(users == null) return "error";
+
+		if (users == null)
+			return "error";
 		model.addAttribute("users", users);
-		return "results"; 
+		return "results";
 	}
-	
 
 	@PostMapping("/saveuser")
-	public String saveUserForm(@ModelAttribute("user") User user, Model model)
-	{
+	public String saveUserForm(@ModelAttribute("user") User user, Model model) {
 		repo.save(user);
 		model.addAttribute("user", user);
-		return "index"; 
+		return "index";
 	}
-	
-	// Code for printing model attributes to console -- Daniel
-	/*Map<String, Object> y = model.asMap();
-    for (Entry<String, Object> entry : y.entrySet())  
-        System.out.println("Key = " + entry.getKey() + 
-                         ", Value = " + entry.getValue()); */
 
-	//launch for editing expense
-	@GetMapping("/editExpense/{id}")
+	// Code for printing model attributes to console -- Daniel
+	/*
+	 * Map<String, Object> y = model.asMap(); for (Entry<String, Object> entry :
+	 * y.entrySet()) System.out.println("Key = " + entry.getKey() + ", Value = " +
+	 * entry.getValue());
+	 */
+
+	// launch for editing expense
+/*	@GetMapping("/editExpense/{id}")
 	public String editExpenseForm(@PathVariable("id") long id, Model model) {
 		User usr = repo.findById(id).orElse(null);
-		if(usr == null) return "error"; // error page goes here
+		if (usr == null)
+			return "error"; // error page goes here
 		model.addAttribute("user", usr);
-		return "edit-user"; 
+		return "edit-user";
 	}
-	
+
 	@PostMapping("/saveExpense")
-	public String saveExpenseForm(@ModelAttribute("user") User user, Model model)
-	{
+	public String saveExpenseForm(@ModelAttribute("user") User user, Model model) {
 		repo.save(user);
 		model.addAttribute("user", user);
-		return "results"; 
+		return "results";
 	}
+*/
+
+
+@PostMapping("editExpense/{id}")
+public String editExpense(@PathVariable long id, @ModelAttribute("user") User user, Model model) {
+
+    user.printAll();
+    repo.save(user);
+
+    return "results";
+}
+
+
+@RequestMapping(path = "editExpense/{id}", method = RequestMethod.GET)
+public String document(@PathVariable long id, Model model) {
+
+	User us = repo.findById(id).orElse(null);
+
+    model.addAttribute("id", id);
+    model.addAttribute("user", us);
+
+    return "edit-expense";
+}
+	
 	
 	@GetMapping("/deleteExpense/{id}")
 	public String deleteExpenseForm(@PathVariable("id") long id, Model model) {
 		User usr = repo.findById(id).orElse(null);
-		if(usr == null) return "error"; // error page goes here
+		if (usr == null)
+			return "error"; // error page goes here
 		repo.delete(usr);
 		model.addAttribute("users", repo.findAll());
-		return "results"; 
+		return "results";
 	}
-	
+
 	@GetMapping("/searchExpense")
-	public String searchExpense(Model model) 
-	{
-		return "search-expense"; 
+	public String searchExpense(Model model) {
+		return "search-expense";
 	}
-	
+
 	@RequestMapping(value = "/searchExpense", method = RequestMethod.POST)
-	public String saveExpenseForm(Model model, @RequestParam(value="searchText") String searchText, @RequestParam(value="searchType") String searchType)
-	{	
-		List<Expense> exp = null;
-		if(searchType.equals("name")) {
-			exp = repo.findByName(searchText);
-		} else if(searchType.equals("category")) {
-			exp = repo.findByCategory(searchText);
+	public String saveExpenseForm(Model model, @RequestParam(value = "searchText") String searchText,
+			@RequestParam(value = "searchType") String searchType) {
+		List<User> exp = repo.findAll();
+		for (int i = exp.size() - 1; i >= 0; i--) {
+			boolean found = false;
+			for (int j = 0; j < exp.get(i).getExpenses().size(); j++) {
+				if (searchType.equals("name")) {
+					if (exp.get(i).getExpense(j).getName().equals(searchText))
+						found = true;
+				} else if (searchType.equals("category")) {
+					if (exp.get(i).getExpense(j).getCategory().getName().equals(searchText))
+						found = true;
+				}
+				if (found)
+					break;
+			}
+			if (!found) {
+				// System.out.println("Removing " + exp.get(i) + " for "+ searchText + ".");
+				exp.remove(i);
+			}
 		}
-		
-		if(exp == null) return "error";
-		model.addAttribute("exp", exp);
-		return "results"; 
+		if (exp == null)
+			return "error";
+		model.addAttribute("users", exp);
+		return "results";
 	}
 }
